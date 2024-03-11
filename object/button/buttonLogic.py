b70514a31cec222b4333e7597569b5fba1ec1bf6
Loïc Dornel <creeperstone72.redstoner@gmail.com>
2024-03-11 17:07:59 +0100

    Bouton Raspberry

--- object/button/buttonLogic.py
+++ object/button/buttonLogic.py
@@ -1,5 +1,8 @@
+import time
 from abc import ABC, abstractmethod
 import keyboard
+from gpiozero import Button
+
 
 class ButtonLogic(ABC):
 
@@ -17,3 +20,20 @@ class KeyBoardButtonLogic(ButtonLogic):
     def initEvents(self, handle_press_method, handle_release_method):
         keyboard.on_press_key(self.key, handle_press_method)
         keyboard.on_release_key(self.key, handle_release_method)
+
+
+class RaspberryButtonLogic(ButtonLogic):
+    def __init__(self, pin, pull_up=True, threshold=1):
+        self._button = Button(pin, pull_up=pull_up)
+        self._threshold = threshold
+
+    def initEvents(self, on_press_method, on_release_method):
+        self._button.wait_for_press()
+        begin = time.time()
+        self._button.wait_for_release()
+        press_duration = time.time() - begin
+
+        if press_duration < self._threshold:
+            on_press_method()
+        else:
+            on_release_method()
--- service/board.py
+++ service/board.py
@@ -1,15 +1,16 @@
 from object.button import Button, LightButton
-from object.button.buttonLogic import KeyBoardButtonLogic
+from object.button.buttonLogic import RaspberryButtonLogic
 from core import Service
+
 
 @Service
 class Board:
 
     def __init__(self):
-        self.mode_button = LightButton(KeyBoardButtonLogic('m'))
-        self.action_button = Button(KeyBoardButtonLogic('a'))
-        self.camera = None #TODO créer la classe Camera
-        self.screen = None #TODO créer la classe Screen
+        self.mode_button = LightButton(RaspberryButtonLogic(6))
+        self.action_button = Button(RaspberryButtonLogic(16, False))
+        self.camera = None  # TODO créer la classe Camera
+        self.screen = None  # TODO créer la classe Screen
 
 
 if __name__ == '__main__':
